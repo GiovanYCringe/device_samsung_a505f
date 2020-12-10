36d49e5cce4e12b5da8067608004fda0773d673a
Enzo Vieira | YukoSky <daffetyxd@gmail.com>
2020-12-10 14:28:40 -0300

    a50dd: Make stable
    Signed-off-by: Enzo Vieira | YukoSky <daffetyxd@gmail.com>

--- build_ofox.sh
+++ build_ofox.sh
@@ -27,7 +27,7 @@ export FOX_R11="1"
 export USE_CCACHE="1"
 export OF_FLASHLIGHT_ENABLE="0"
 export OF_SCREEN_H="2340"
-export FOX_BUILD_TYPE="Beta"
+export FOX_BUILD_TYPE="Stable"
 export FOX_ADVANCED_SECURITY="1"
 export LZMA_RAMDISK_TARGETS="recovery"
 
